c3fdf8157a905136ac89ff0507f8d36fadad997a
Gonzalo Vazquez <gonzalovazquezfuentes@gmail.com>
2024-09-23 23:54:56 -0300

    DerivarUsuarios Testing completo

--- src/test/java/DerivarUsuariosTest.java
+++ src/test/java/DerivarUsuariosTest.java
@@ -5,7 +5,6 @@
 
 import derivadores.DerivarUsuarios;
 import logica.Usuario;
-import logica.Usuarios;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,80 +16,83 @@ public class DerivarUsuariosTest {
 
     @Test
     public void testConstructorInicializaCorrectamente() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Alta", "user1;;;pass1");
+        DerivarUsuarios derivador = new DerivarUsuarios("Alta", "77777777;;;pass1");
 
         assertEquals("Alta", derivador.getOperacion());
-        assertEquals("user1;;;pass1", derivador.getMensaje());
+        assertEquals("77777777;;;pass1", derivador.getMensaje());
         assertNotNull(derivador.getUsuarios());
     }
 
     @Test
     public void testValidarDatosUsuarioVacio() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Login", "user1;;;pass1");
+        DerivarUsuarios derivador = new DerivarUsuarios("Login", "77777777;;;pass1");
+
         assertFalse(derivador.validarDatos("", "password"));
     }
 
     @Test
     public void testValidarDatosContraseniaVacia() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Login", "user1;;;pass1");
+        DerivarUsuarios derivador = new DerivarUsuarios("Login", "77777777;;;pass1");
 
         assertFalse(derivador.validarDatos("usuario", ""));
     }
 
     @Test
     public void testDerivarUsuariosOperacionAlta() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Alta", "user1;;;pass1");
-        Usuarios usuarios = new Usuarios();
-        usuarios.perisistirUsuarios();
-        derivador.setUsuarios(usuarios);
 
+        DerivarUsuarios derivador = new DerivarUsuarios("Alta", "77777777;;;pass1");
+
+        if (derivador.getUsuarios().existeUsuario("77777777")) {
+            derivador.getUsuarios().eliminarUsuario("77777777");
+        }
         String resultado = derivador.derivarUsuarios();
         assertEquals("Usuario creado con éxito,;,200", resultado);
     }
 
     @Test
     public void testDerivarUsuariosOperacionLoginCorrecto() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Login", "user1;;;pass1");
-        Usuarios usuarios = new Usuarios();
-        usuarios.perisistirUsuarios();
-        usuarios.agregarUsuario(new Usuario("user1", "pass1", "estudiante"));
-        derivador.setUsuarios(usuarios);
+        DerivarUsuarios derivador = new DerivarUsuarios("Login", "77777777;;;pass1");
+        if (!derivador.getUsuarios().existeUsuario("77777777")) {
+            derivador.getUsuarios().agregarUsuario(new Usuario("77777777", "pass1", "estudiante"));
+        }
 
         String resultado = derivador.derivarUsuarios();
         assertEquals("estudiante,;,200", resultado);
+        derivador.getUsuarios().eliminarUsuario("77777777");
     }
 
     @Test
     public void testDerivarUsuariosOperacionLoginInvalido() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Login", "user1;;;wrongpass");
-        Usuarios usuarios = new Usuarios();
-        usuarios.perisistirUsuarios();
-        usuarios.agregarUsuario(new Usuario("user1", "pass1", "estudiante"));
-        derivador.setUsuarios(usuarios);
+        DerivarUsuarios derivador = new DerivarUsuarios("Login", "77777777;;;wrongpass");
+        if (!derivador.getUsuarios().existeUsuario("77777777")) {
+            derivador.getUsuarios().agregarUsuario(new Usuario("77777777", "pass1", "estudiante"));
+        }
 
         String resultado = derivador.derivarUsuarios();
         assertEquals("Usuario y/o contraseña incorrectos,;,400", resultado);
+        derivador.getUsuarios().eliminarUsuario("77777777");
     }
 
     @Test
     public void testDerivarCambioPasswordCorrecto() {
-        DerivarUsuarios derivador = new DerivarUsuarios("CambioPassword", "user1;;;newpass");
-        Usuarios usuarios = new Usuarios();
-        usuarios.perisistirUsuarios();
-        usuarios.agregarUsuario(new Usuario("user1", "oldpass", "estudiante"));
-        derivador.setUsuarios(usuarios);
+        DerivarUsuarios derivador = new DerivarUsuarios("CambioPassword", "77777777;;;newpass");
+        if (!derivador.getUsuarios().existeUsuario("77777777")) {
+            derivador.getUsuarios().agregarUsuario(new Usuario("77777777", "oldpass", "estudiante"));
+        }
 
         String resultado = derivador.derivarUsuarios();
         assertEquals("Cambio con éxito,;,200", resultado);
-        assertEquals("newpass", usuarios.obtenerUsuario("user1").getContrasenia());
+        assertEquals("newpass", derivador.getUsuarios().obtenerUsuario("77777777").getContrasenia());
+        derivador.getUsuarios().eliminarUsuario("77777777");
     }
 
     @Test
     public void testDerivarCambioPasswordUsuarioNoExiste() {
-        DerivarUsuarios derivador = new DerivarUsuarios("CambioPassword", "user1;;;newpass");
-        Usuarios usuarios = new Usuarios(); // No se agrega el usuario
-        usuarios.perisistirUsuarios();
-        derivador.setUsuarios(usuarios);
+        DerivarUsuarios derivador = new DerivarUsuarios("CambioPassword", "77777777;;;newpass");
+
+        if (derivador.getUsuarios().existeUsuario("77777777")) {
+            derivador.getUsuarios().eliminarUsuario("77777777");
+        }
 
         String resultado = derivador.derivarUsuarios();
         assertEquals("No existe usuario,;,500", resultado);
@@ -98,7 +100,7 @@ public class DerivarUsuariosTest {
 
     @Test
     public void testDerivarValidezNombreUsuarioCorrecto() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Validez", "12345678");
+        DerivarUsuarios derivador = new DerivarUsuarios("Validez", "77777777");
 
         String resultado = derivador.derivarValidezNombreUsuario();
         assertEquals("Formato de CI correcto,;,200", resultado);
@@ -114,22 +116,24 @@ public class DerivarUsuariosTest {
 
     @Test
     public void testDerivarExistenciaUsuarioCorrecto() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Existencia", "12345678");
-        Usuarios usuarios = new Usuarios();
-        usuarios.perisistirUsuarios();
-        usuarios.agregarUsuario(new Usuario("12345678", "pass1", "estudiante"));
-        derivador.setUsuarios(usuarios);
+        DerivarUsuarios derivador = new DerivarUsuarios("Existencia", "77777777");
+
+        if (!derivador.getUsuarios().existeUsuario("77777777")) {
+            derivador.getUsuarios().agregarUsuario(new Usuario("77777777", "pass1", "estudiante"));
+        }
 
         String resultado = derivador.derivarUsuarios();
         assertEquals("Usuario existente en el sistema,;,200", resultado);
+        derivador.getUsuarios().eliminarUsuario("77777777");
     }
 
     @Test
     public void testDerivarExistenciaUsuarioNoExiste() {
-        DerivarUsuarios derivador = new DerivarUsuarios("Existencia", "12345678");
-        Usuarios usuarios = new Usuarios(); // No se agrega el usuario
-        usuarios.perisistirUsuarios();
-        derivador.setUsuarios(usuarios);
+        DerivarUsuarios derivador = new DerivarUsuarios("Existencia", "77777777");
+
+        if (derivador.getUsuarios().existeUsuario("77777777")) {
+            derivador.getUsuarios().eliminarUsuario("77777777");
+        }
 
         String resultado = derivador.derivarUsuarios();
         assertEquals("Usuario NO existe,;,400", resultado);
